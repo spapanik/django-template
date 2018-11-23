c4453d4d25f621cabc016f618082c92cac17995d
Steph Papanik <spapanik21@gmail.com>
2018-11-23 16:23:24 +0000

    Remove non-existing middleware

--- settings/django.py
+++ settings/django.py
@@ -42,7 +42,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "authentication.middleware.JWTAuthMiddleware",
 ]
 
 TEMPLATES = [
