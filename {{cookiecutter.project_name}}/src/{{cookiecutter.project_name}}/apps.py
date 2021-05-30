5e6abb9edaaf675e51920fe65be0d057a1284115
Stephanos Kuma <stephanos@kuma.ai>
2021-05-30 13:51:13 +0100

    Move urls and wsgi to new app

---
+++ {{cookiecutter.project_name}}/src/{{cookiecutter.project_name}}/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class {{cookiecutter.project_name}}AppConfig(AppConfig):
+    name = "{{cookiecutter.project_name}}"
+    verbose_name = "{{cookiecutter.project_name}}"
