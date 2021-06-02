f5259b8d3f0d7a8e00faf8f1c1ad5added914760
Stephanos Kuma <stephanos@kuma.ai>
2021-06-02 23:18:05 +0100

    Use an email backend by default in dev

--- {{cookiecutter.project_name}}/src/settings/dev.py
+++ {{cookiecutter.project_name}}/src/settings/dev.py
@@ -5,6 +5,9 @@ DEBUG = True
 ROOT_URLCONF = "sigil.urls_dev"
 ALLOWED_HOSTS = ["*"]
 
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_FILE_PATH = BASE_DIR.joinpath("emails")  # noqa: F405
+
 SECRET_KEY = "Insecure:fXP7kny5q3oKDV6_yBjs-keX6oZfRqC9pz--LDJ42r8"
 
 MIDDLEWARE += [  # noqa: F405
