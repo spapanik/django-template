26665018758d0464a046ba92e719d93619a16605
Stephanos Kuma <stephanos@kuma.ai>
2021-09-21 09:10:39 +0100

    Fix failing tests

---
+++ {{cookiecutter.project_name}}/tests/conftest.py
@@ -0,0 +1,14 @@
+from pytest import fixture
+
+from django.test import Client
+
+
+class HttpTestClient(Client):
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault("HTTP_X_FORWARDED_PROTO", "https")
+        super().__init__(*args, **kwargs)
+
+
+@fixture
+def http_client():
+    return HttpTestClient()
--- {{cookiecutter.project_name}}/tests/test_urls.py
+++ {{cookiecutter.project_name}}/tests/test_urls.py
@@ -1,8 +1,4 @@
-from django.test import Client
-
-
-def test_admin():
-    client = Client()
+def test_admin(http_client):
     response = client.get("/admin", follow=True)
     expected_redirects = [("/admin/", 301), ("/admin/login/?next=/admin/", 302)]
     assert response.redirect_chain == expected_redirects
