325e4d3f63fc9c316b34f795d400bbf60bbfe122
Stephanos Kuma <stephanos@kuma.ai>
2021-09-30 17:09:21 +0100

    Add a simple homepage

--- {{cookiecutter.project_name}}/src/urls.py
+++ {{cookiecutter.project_name}}/src/urls.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
-urlpatterns = [path("admin/", admin.site.urls)]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("home.urls", namespace="home")),
+]
