43193b6ed685aa3157b80eb221d3eb1f33e99073
Stephanos Kuma <stephanos@kuma.ai>
2022-07-22 11:20:13 +0100

    Add date util functions

---
+++ {{cookiecutter.project_name}}/src/lib/datetime.py
@@ -0,0 +1,28 @@
+import typing as tp
+from datetime import datetime
+from zoneinfo import ZoneInfo, available_timezones
+
+from lib.data.timezones import (
+    DEPRECATED_TIMEZONES,
+    NON_IANA_TIMEZONES,
+    PROPOSED_TIMEZONES,
+)
+
+UTC = ZoneInfo("UTC")
+
+
+def get_timezones() -> tp.Set[str]:
+    unavailable_timezones = set.union(
+        DEPRECATED_TIMEZONES, NON_IANA_TIMEZONES, PROPOSED_TIMEZONES
+    )
+    return available_timezones() - unavailable_timezones
+
+
+def now(tz_info: ZoneInfo = UTC) -> datetime:
+    return datetime.now(UTC).astimezone(tz_info)
+
+
+def as_timezone(dt: datetime, tz_info: ZoneInfo = UTC) -> datetime:
+    if dt.tzinfo is None:
+        return dt.replace(tzinfo=tz_info)
+    return dt.astimezone(tz_info)
