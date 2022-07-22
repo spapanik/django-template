4c06ebccd440e1bfccb9ee9da54a090a52feeb24
Stephanos Kuma <stephanos@kuma.ai>
2022-07-22 11:13:28 +0100

    Remove unnecessary files

--- {{cookiecutter.project_name}}/src/lib/admin.py
+++
@@ -1,7 +0,0 @@
-def override(**kwargs):
-    def wrapped_function(func):
-        for key, value in kwargs.items():
-            setattr(func, key, value)
-        return func
-
-    return wrapped_function
--- {{cookiecutter.project_name}}/src/lib/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class LibAppConfig(AppConfig):
-    name = "lib"
-    verbose_name = "Lib"
